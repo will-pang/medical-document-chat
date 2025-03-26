b79413196b557a84b8cb4da1b25e444487c68d19
bayesandmarkov <william.pang@aya.yale.edu>
2025-03-26 15:51:19 -0700

    Minor update

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-[calc(100dvh-73px)] gap-8 px-4 bg-background">
       <div className="max-w-2xl text-center space-y-8">
         <h1 className="text-6xl font-bold tracking-tight">
-          Introducing ⚕️MedDocChat.
+          ⚕️MedDocChat.
         </h1>
         <h3 className="text-2xl font-bold tracking-tight">
           Time to start chatting with medical documents.
